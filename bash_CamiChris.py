7fa68810a042bf8f465918c50a3d8a3dd140459a
camidorego <camiladoregobarros@gmail.com>
2023-01-17 10:47:42 -0300

    correccion de errores

--- bash_CamiChris.py
+++ bash_CamiChris.py
@@ -4,11 +4,11 @@ import os
 import time
 import signal
 import shutil
-<<<<<<< HEAD
 # import distutils.dir_util
 import ftplib
 import logging
 import psutil # instalar desde la terminal -> pip install psutil
+import errno
 
 # ---LOGS--- #
 	#IMPORTANTE:: SE DEBE CREAR LA CARPETA /var/log/shell ANTES
@@ -89,7 +89,7 @@ def cmdListar(cadena):
 #Fucnion para crear un directorio -> creardir [path]
 def cmdCrearDir(cadena):
 	cadena=cadena.split(sep=' ')
-	for i in range(0, len(cadena)-1):
+	for i in range(0, len(cadena)):
 		try:
 			os.mkdir(os.path.abspath(cadena[i])) # se crea el directorio
 			print("se creo el directorio correctamente")
@@ -188,467 +188,6 @@ def cmdEjecutar(cadena):
 		# Se guarda en el log de errores
 		mensaje = "ejecutar: comando no encontrado" + cadena
 		logErrores(mensaje)
-=======
-import distutils.dir_util
-import ftplib
-import logging
-import psutil # instalar desde la terminal -> pip install psutil
-
-def cmdCopiar(cadena):
-	cadena = cadena.split(sep = ' ')
-	origen = cadena[0]
-	destino = cadena[1]
-
-	#copiar archivo
-	if os.path.isfile(origen):
-		#creamos directorio si no existe
-		if (os.path.exists(destino) == False):
-			os.makedirs(destino)    
-		try:
-			shutil.copy(origen,destino)
-			print("Archivo copiado exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "copiar: se copio exitosamente"  
-			logMovimientos(mensaje)
-
-		# Se comprueba si el origen y el destino son iguales
-		except shutil.SameFileError:
-			print("El archivo a copiar es el mismo que el de destino.")
-
-			# Se guarda en el log de errores
-			mensaje = "copiar: El archivo a copiar es el mismo que el de destino."
-			logErrores(mensaje)
-
-		except PermissionError:
-			print("Permission denied.")
-
-			# Se guarda en el log de errores
-			mensaje = "copiar: Permission denied."
-			logErrores(mensaje)
-
-		except:
-			print("Error al copiar archivo.")
-			# Se guarda en el log de errores
-			mensaje = "copiar: Error al copiar archivo."
-			logErrores(mensaje)
- 
-	#copiar directorio
-	elif os.path.isdir(origen):
-
-		if (os.path.exists(origen) == False):
-			os.makedirs(destino)
-	
-		archivos=os.listdir(origen)
-		for fname in archivos:
-			shutil.copy2(os.path.join(origen,fname), destino) #solo copia archivos dentro del directorio, carpetas no 
-			print("Copiado exitosamente")
-
-			# Se guarda en el log de errores
-			mensaje = "copiar: Error al copiar directorio."
-			logErrores(mensaje)
-
-	if(os.path.isfile(origen) == False and os.path.isdir(origen) == False):
-		print("No existe el archivo o directorio.")
-		# Se guarda en el log de errores
-		mensaje = "copiar: No existe el archivo o directorio."
-		logErrores(mensaje)
-
-	
-def cmdRenombrar(cadena): #Funcion para renombrar directorios
-	cadena = cadena.split(sep = ' ') #se separan los paths en arrays
-	original = cadena[0]
-	nuevo = cadena[1] 
-
-	if os.path.isfile(original): #se verifica si es un archivo
-		try :
-			os.rename(original,nuevo)
-			print("Renombrado exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "renombrar: se renombro exitosamente"  
-			logMovimientos(mensaje)
-
-		except:
-			print("Error al renombrar.")
-			# Se guarda en el log de errores
-			mensaje = "renombrar: Error al renombrar."
-			logErrores(mensaje)
-
-
-	elif os.path.isdir(original): #se verifica si es un directorio
-		try :
-			os.rename(original,nuevo)
-			print("Renombrado exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "renombrar: se renombro exitosamente"  
-			logMovimientos(mensaje)
-
-		except:
-			print("Error al renombrar.")
-			# Se guarda en el log de errores
-			mensaje = "renombrar: Error al renombrar."
-			logErrores(mensaje)
-
-	elif(os.path.isfile(original) == False and os.path.isdir(original) == False): # si no es un directorio o archivo lanza un print de error
-		print("No existe el archivo o directorio.")
-		# Se guarda en el log de errores
-		mensaje = "renombrar: No existe el archivo o directorio."
-		logErrores(mensaje)
-
-
-def cmdMover(cadena): #Funcion para mover archivos o directorios
-	cadena = cadena.split(sep=' ') #se separan los paths en arrays
-	oldPath = cadena[0]
-	newPath = cadena[1]
-
-	if os.path.exists(oldPath):
-		try :
-			shutil.move(oldPath,newPath) #se actualiza el path
-			print("Se movio exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "mover: se movio exitosamente"  
-			logMovimientos(mensaje)
-
-		except:
-			print("Error al mover.")
-			# Se guarda en el log de errores
-			mensaje = "mover: Error al mover."
-			logErrores(mensaje)
-
-
-	else:
-		print("No existe el archivo o directorio.")	
-		# Se guarda en el log de errores
-		mensaje = "mover: No existe el archivo o directorio."
-		logErrores(mensaje)
-
-#Funcion para buscar una cadena dentro de un archivo  -> buscar "cadena a buscar"
-def cmdBuscar(cadena):
-	cadena = cadena.split(sep=' ') #se separan los paths en arrays
-	path = cadena[0]
-	palabra = cadena[1]
-
-	if(os.path.isfile(path)):
-		a = open(path,"r") # se abre el archivo
-		texto = a.read()  # se guarda el contenido en texto 
-
-		if palabra in texto: # se busca la cadena
-			print("La cadena existe en el archivo.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "buscar: se encontro la cadena en el archivo"  
-			logMovimientos(mensaje)
-		else:
-			print("La cadena no existe en el archivo.")
-			# Se guarda en el log de errores
-			mensaje = "buscar: La cadena no existe en el archivo."
-			logErrores(mensaje)
-
-			# Se guarda en el log de movimientos
-			mensaje = "buscar: no se encontro la cadena en el archivo"  
-			logMovimientos(mensaje)
-	else:
-		print("No existe el archivo.")
-		# Se guarda en el log de errores
-		mensaje = "buscar: No existe el archivo."
-		logErrores(mensaje)
-
-
-
-
-# ---LOGS--- #
-	#IMPORTANTE:: SE DEBE CREAR LA CARPETA /var/log/shell ANTES
-def logTransferencias(status, mensaje):
-	# Configuramos el logger
-	logging.basicConfig(filename='/var/log/shell/shell_transferencias.log',
-						filemode='w',
-						level=logging.INFO,
-						format='%(asctime)s %(message)s')
-
-	# Creamos el objeto logger
-	logger = logging.getLogger()
-
-	if status==1:
-		# Transferencia exitosa
-		logger.info(mensaje) 
-	elif status == 0:
-		# Transferencia con error
-		logger.info(mensaje)
-
-def logErrores(mensaje):
-	# Configuramos el logger
-	logging.basicConfig(filename='/var/log/shell/sistema_error.log',
-						filemode='w',
-						level=logging.INFO,
-						format='%(asctime)s %(message)s')
-	# Creamos el objeto logger
-	logger = logging.getLogger()
-
-	# Se escribe el mensaje de error
-	logger.info(mensaje)
-
-def logMovimientos(mensaje):
-	# Configuramos el logger
-	logging.basicConfig(filename='/var/log/shell/registro_movimientos.log',
-						filemode='w',
-						level=logging.INFO,
-						format='%(asctime)s %(message)s')
-	# Creamos el objeto logger
-	logger = logging.getLogger()
-
-	# Se escribe el mensaje 
-	logger.info(mensaje)
-
-
-
-# ---COMANDOS--- #
-#Funcion para listar un directorio -> listar o listar [path]
-def cmdListar(cadena):
-	if(len(cadena) == 6): # se listan los archivos del path actual
-		path = os.getcwd() # se obtiene el path actual
-		lista = os.listdir(path) # se obtienen los archivos
-		print("Archivos en ", path, ":")
-		print(lista)
-
-		# Se guarda en el log de movimientos
-		mensaje = "listar: se listaron los archivos de " + path 
-		logMovimientos(mensaje)
-	else:
-		cadena = cadena.split(sep=' ')
-		path = cadena[1] # se guarda el path introducido
-		if os.path.exists(path):
-			lista = os.listdir(path) # se obtienen los archivos
-			print("Archivos en ", path, ":")
-			print(lista)
-
-			# Se guarda en el log de movimientos
-			mensaje = "listar: se listaron los archivos de " + path 
-			logMovimientos(mensaje)
-		else:
-			print("El path ingresado es incorrecto")
-
-			# Se guarda en el log de errores
-			mensaje = "listar: El path ingresado es incorrecto"
-			logErrores(mensaje)
-
-#Fucnion para crear un directorio -> creardir [path]
-def cmdCrearDir(cadena):
-	try:
-		os.mkdir(os.path.abspath(cadena)) # se crea el directorio
-		print("Se creo el directorio correctamente")
-
-		# Se guarda en el log de movimientos
-		mensaje = "creardir: se creo un directorio en " + cadena
-		logMovimientos(mensaje)
-	except FileExistsError:
-		print("El directorio ya existe")
-
-		# Se guarda en el log de errores
-		mensaje = "creardir: El directorio ya existe"
-		logErrores(mensaje)
-
-#Funcion para ir a un directorio -> ir [path]
-def cmdIr(path):
-	try: os.path.exists(path): # se verifica el path introducido
-		os.chdir(os.path.abspath(path))  # se cambia de directorio al path solicitado
-		path_n = os.getcwd() # se obtiene la nueva ubicacion 
-		print("El path actual es: ", path_n)
-
-		# Se guarda en el log de movimientos
-		mensaje = "ir: el usuario se movio desde " + path + " a " + path_n
-		logMovimientos(mensaje)
-	except:
-		print("El path introducido es incorrecto")
-
-		# Se guarda en el log de errores
-		mensaje = "ir: El path ingresado es incorrecto"
-		logErrores(mensaje)
-
-#Funcion para obtener el path del directorio actual -> pwd
-def cmdPwd():
-	path = os.getcwd() # se obtiene el path actual
-	print("El path actual es: ", path) # se imprime
-
-	# Se guarda en el log de movimientos
-	mensaje = "pwd: se mostro " + path 
-	logMovimientos(mensaje)
-
-#Funcion para cambiar los permisos de un directorio -> permisos [permisos en Octal] [path]
-def cmdPermisos(cadena):
-	cadena = cadena.split(sep = ' ')
-	permisos = cadena[0]
-	path = cadena[1]
-	try: os.path.exists(path):
-		os.chmod(os.path.abspath(path), permisos)
-
-		# Se guarda en el log de movimientos
-		mensaje = "permisos: se cambiaron los permisos de " + path 
-		logMovimientos(mensaje)
-	except:
-		print("El path introducido es incorrecto")
-
-		# Se guarda en el log de errores
-		mensaje = "permisos: El path ingresado es incorrecto"
-		logErrores(mensaje)
-
-#Funcion que imprime el historial de comandos
-def cmdHistory(comandos): 
-	print("El historial de comandos es:")
-	for i in range(0, len(comandos)):
-		print(i+1, ": ", comandos[i])
-
-	# Se guarda en el log de movimientos
-	mensaje = "historial: se mostraron los comandos del historial " 
-	logMovimientos(mensaje)
-
-#Funcion para ejecutar otros comandos de linux
-def cmdEjecutar(cadena):
-	try:
-		subprocess.run(cadena.split())
-
-		# Se guarda en el log de movimientos
-		mensaje = "ejecutar: se ejecuto " + cadena 
-		logMovimientos(mensaje)
-	except:
-		print("Comando no encontrado")
-
-		# Se guarda en el log de errores
-		mensaje = "ejecutar: comando no encontrado" + cadena
-		logErrores(mensaje)
-
-#Funcion para copiar un archivo o directorio -> copiar
-def cmdCopiar(cadena):
-	cadena = cadena.split(sep = ' ')
-	origen = cadena[0]
-	destino = cadena[1]
-
-	#copiar archivo
-	if os.path.isfile(origen):
-		#creamos directorio si no existe
-		if (os.path.exists(destino) == False):
-			os.makedirs(destino)    
-		try:
-			shutil.copy(origen,destino)
-			print("Archivo copiado exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "copiar: se copio exitosamente"  
-			logMovimientos(mensaje)
-
-		# Se comprueba si el origen y el destino son iguales
-		except shutil.SameFileError:
-			print("El archivo a copiar es el mismo que el de destino.")
-
-			# Se guarda en el log de errores
-			mensaje = "copiar: El archivo a copiar es el mismo que el de destino."
-			logErrores(mensaje)
-
-		except PermissionError:
-			print("Permission denied.")
-
-			# Se guarda en el log de errores
-			mensaje = "copiar: Permission denied."
-			logErrores(mensaje)
-
-		except:
-			print("Error al copiar archivo.")
-			# Se guarda en el log de errores
-			mensaje = "copiar: Error al copiar archivo."
-			logErrores(mensaje)
- 
-	#copiar directorio
-	elif os.path.isdir(origen):
-		try:
-			distutils.dir_util.copy_tree(origen,destino)
-		except:
-			print("Error al copiar directorio.")
-
-			# Se guarda en el log de errores
-			mensaje = "copiar: Error al copiar directorio."
-			logErrores(mensaje)
-
-	if(os.path.isfile(origen) == False and os.path.isdir(origen) == False):
-		print("No existe el archivo o directorio.")
-		# Se guarda en el log de errores
-		mensaje = "copiar: No existe el archivo o directorio."
-		logErrores(mensaje)
-
-
-#Funcion para renombrar un archivo o directorio -> renombrar
-def cmdRenombrar(cadena):
-	cadena = cadena.split(sep = ' ')
-	original = cadena[0]
-	nuevo = cadena[1] 
-
-	if os.path.isfile(original):
-		try :
-			os.rename(original,nuevo)
-			print("Renombrado exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "renombrar: se renombro exitosamente"  
-			logMovimientos(mensaje)
-
-		except:
-			print("Error al renombrar.")
-			# Se guarda en el log de errores
-			mensaje = "renombrar: Error al renombrar."
-			logErrores(mensaje)
-
-
-	elif os.path.isdir(original):
-		try :
-			os.rename(original,nuevo)
-			print("Renombrado exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "renombrar: se renombro exitosamente"  
-			logMovimientos(mensaje)
-
-		except:
-			print("Error al renombrar.")
-			# Se guarda en el log de errores
-			mensaje = "renombrar: Error al renombrar."
-			logErrores(mensaje)
-
-	elif(os.path.isfile(original) == False and os.path.isdir(original) == False):
-		print("No existe el archivo o directorio.")
-		# Se guarda en el log de errores
-		mensaje = "renombrar: No existe el archivo o directorio."
-		logErrores(mensaje)
-
-
-# Funcion para mover un archivo o directorio -> mover
-def cmdMover(cadena):
-	cadena = cadena.split(sep=' ')
-	oldPath = cadena[0]
-	newPath = cadena[1]
-
-	if os.path.exists(oldPath):
-		try :
-			shutil.move(oldPath,newPath)
-			print("Se movio exitosamente.")
-
-			# Se guarda en el log de movimientos
-			mensaje = "mover: se movio exitosamente"  
-			logMovimientos(mensaje)
-
-		except:
-			print("Error al mover.")
-			# Se guarda en el log de errores
-			mensaje = "mover: Error al mover."
-			logErrores(mensaje)
-
-
-	else:
-		print("No existe el archivo o directorio.")	
-		# Se guarda en el log de errores
-		mensaje = "mover: No existe el archivo o directorio."
-		logErrores(mensaje)
->>>>>>> 271fbaf803030341e7a6d605583b6b46839b6d8c
 
 #Funcion para buscar una cadena dentro de un archivo  -> buscar "cadena a buscar"
 def cmdBuscar(cadena):
@@ -780,11 +319,10 @@ def cmdKill():
 		opcion1 = int(input("OPCIONES: \n 1)Ver lista de procesos corriendo \n 2)Ya tengo el PID del proceso que quiero terminar \n Elije una opcion: "))
 		if opcion1 == 1:
 			for proc in psutil.process_iter():
-<<<<<<< HEAD
 				try:
 					processName = proc.name()
 					processID = proc.pid
-					print(processName , ' ::: ', processID)
+					print('PROCESO: ', processName , ' :::  PID = ', processID)
 				except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
 					pass
 		elif opcion1 == 2:
@@ -793,27 +331,12 @@ def cmdKill():
 			print("Opcion fuera de rango, vuelva a ingresar")
 	
 	pid=int(input("Ingresa el pid del proceso: "))
-=======
-        			try:
-            				processName = proc.name()
-            				processID = proc.pid
-            				print(processName , ' ::: ', processID)
-        			except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            				pass
-        	elif opcion1 == 2:
-        		break
-        	else:
-        		print("Opcion fuera de rango, vuelva a ingresar")
-   
-    	pid=int(input("Ingresa el pid del proceso: "))
->>>>>>> 271fbaf803030341e7a6d605583b6b46839b6d8c
 	os.kill(pid, signal.SIGKILL)
 	print("Se termino el proceso")
 
 	# Se guarda en el log de movimientos
 	mensaje = "kill: se termino el proceso " + str(pid) 
 	logMovimientos(mensaje)
-<<<<<<< HEAD
 
 # Funcion para copiar archivo/s y/o directorio/s a un path especifico -> copiar [archivos/directorios] [path_destino]
 def cmdCopiar(cadena):
@@ -826,7 +349,7 @@ def cmdCopiar(cadena):
 	for i in range(0, len(cadena)-1):
         # Si lo que se quiere copiar es un directorio
 		try:
-			shutil.copytree(cadena[i], destino)
+			shutil.copytree(cadena[i], destino + "/" + cadena[i])
 			print("Se copio exitosamente el directorio ", cadena[i])
 
             # Se escribe el mensaje en el log
@@ -847,7 +370,7 @@ def cmdCopiar(cadena):
 				print("Error: %s" %err)
 
 				# Se escribe el mensaje en el log
-				msg="copiar:  " + err
+				msg="copiar:  " + str(err)
 				logErrores(msg)
 
 # Funcion para renombrar un archivo o directorio -> renombrar [archivo_original] [archivo_con_nombre_nuevo]
@@ -860,16 +383,16 @@ def cmdRenombrar(cadena):
 	if os.path.exists(original):
 		try :
 			os.rename(original,nuevo)
-			print("Renombrado exitosamente a ") + nuevo
+			print("Renombrado exitosamente a ") + str(nuevo)
 
 			# Se guarda en el log de movimientos
-			mensaje = "renombrar: se renombro exitosamente" + nuevo
+			mensaje = "renombrar: se renombro exitosamente" + str(nuevo)
 			logMovimientos(mensaje)
 
 		except:
-			print("Error al renombrar ") + nuevo
+			print("Error al renombrar ") + str(nuevo)
 			# Se guarda en el log de errores
-			mensaje = "renombrar: Error al renombrar " + nuevo
+			mensaje = "renombrar: Error al renombrar " + str(nuevo)
 			logErrores(mensaje)
 
 
@@ -909,8 +432,6 @@ def cmdMover(cadena):
 			# Se guarda en el log de errores
 			mensaje = "mover: No existe el archivo o directorio " + cadena[i]
 			logErrores(mensaje)
-=======
->>>>>>> 271fbaf803030341e7a6d605583b6b46839b6d8c
 
 
 
@@ -975,7 +496,6 @@ def main():
 		elif (comando[:4]=="kill") :
 			cmdKill()
 			historial.append("kill")
-<<<<<<< HEAD
 		
 		elif(comando[:6]=="copiar"):
 			cmdCopiar(comando[7:])
@@ -988,8 +508,6 @@ def main():
 		elif(comando[:5]=="mover"):
 			cmdMover(comando[6:])
 			historial.append("mover")
-=======
->>>>>>> 271fbaf803030341e7a6d605583b6b46839b6d8c
 			
 
 if __name__ == "__main__":
